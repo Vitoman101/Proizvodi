23b073e6e53e729f66a03d3ce5c7f57d00df4dde
Viktor.Lekić.2403 <viktor.lekic95@gmail.com>
2020-12-10 17:24:01 +0100

    JSON output deo sa kontolerom
    Model, kontroler, view page i json output fajl

---
+++ Proizvodi/Models/Proizvod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proizvodi.Models
+{
+    public class Proizvod
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+        public string Kategorija { get; set; }
+        public string Proizvodjac { get; set; }
+        public string Dobavljac { get; set; }
+        public decimal Cena { get; set; }
+
+    }
+}
